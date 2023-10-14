34ea8dcf3b027df542c843a3efb425eb161783af
Feona8008 <gana.feonamae.10022002@gmail.com>
2023-10-14 12:34:51 +0800

    Add movies and photos

--- BSIT3L-Movies/Controllers/HomeController.cs
+++ BSIT3L-Movies/Controllers/HomeController.cs
@@ -15,9 +15,21 @@ public class HomeController : Controller
         _logger = logger;
         _movies = new List<MovieViewModel>
         {
-            new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
-            new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
-            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+            new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama", Image = "~/images/titanic.jpg" }, 
+            new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller", Image = "~/images/inception.jpg"  },
+            new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama", Image = "~/images/TheShawshankRedemption.jpg"  },
+            new MovieViewModel { Id = 4, Name = "Guardians of the Galaxy", Rating = "8", ReleaseYear = 2014, Genre = "Science Fiction/Action",Image = "~/images/GuardiansOfTheGalaxy.jpg" },
+            new MovieViewModel { Id = 5, Name = "The Nun", Rating = "5", ReleaseYear = 2018, Genre = "Horror", Image = "~/images/TheNun.jpg" },
+            new MovieViewModel { Id = 6, Name = "A Quiet Place", Rating = "7", ReleaseYear = 2018, Genre = "Horror", Image = "~/images/AQuietPlace.jpg" },
+            new MovieViewModel { Id = 7, Name = "John Wick", Rating = "8", ReleaseYear = 2023, Genre = "Action", Image = "~/images/JohnWick.jpg" },
+            new MovieViewModel { Id = 8, Name = "Kill Boksoon", Rating = "5", ReleaseYear = 2023, Genre = "Action", Image = "~/images/KillBoksoon.jpg" },
+            new MovieViewModel { Id = 9, Name = "Polite Society", Rating = "7", ReleaseYear = 2023, Genre = "Action", Image = "~/images/PoliteSociety.jpg" },
+            new MovieViewModel { Id = 10, Name = "The Mother", Rating = "8", ReleaseYear = 2023, Genre = "Action", Image = "~/images/TheMother.jpg" },
+            new MovieViewModel { Id = 11, Name = "Barcelona", Rating = "5", ReleaseYear = 2016, Genre = "Romance", Image = "~/images/Barcelona.jpg" },
+            new MovieViewModel { Id = 12, Name = "Haunted Mansion", Rating = "7", ReleaseYear = 2023, Genre = "Fantasy", Image = "~/images/HauntedMansion.jpg" },
+            new MovieViewModel { Id = 13, Name = "Spy", Rating = "8", ReleaseYear = 2023, Genre = "Fantasy", Image = "~/images/Spy.jpg" },
+            new MovieViewModel { Id = 14, Name = "Barbie", Rating = "5", ReleaseYear = 2023, Genre = "Comedy", Image = "~/images/Barbie.jpg" },
+            new MovieViewModel { Id = 15, Name = "Dream", Rating = "7", ReleaseYear = 2023, Genre = "Comedy", Image = "~/images/Dream.jpg" },
         };
     }
 
--- BSIT3L-Movies/Controllers/MoviesController.cs
+++ BSIT3L-Movies/Controllers/MoviesController.cs
@@ -15,6 +15,18 @@ namespace BSIT3L_Movies.Controllers
                new MovieViewModel { Id = 1, Name = "Titanic", Rating = "5", ReleaseYear = 1997, Genre = "Romance/Drama" },
             new MovieViewModel { Id = 2, Name = "Inception", Rating = "4", ReleaseYear = 2010, Genre = "Science Fiction/Thriller" },
             new MovieViewModel { Id = 3, Name = "The Shawshank Redemption", Rating = "5", ReleaseYear = 1994, Genre = "Drama" },
+            new MovieViewModel { Id = 4, Name = "Guardians of the Galaxy", Rating = "8", ReleaseYear = 2014, Genre = "Science Fiction/Action" },
+            new MovieViewModel { Id = 5, Name = "The Nun", Rating = "5", ReleaseYear = 2018, Genre = "Horror" },
+            new MovieViewModel { Id = 6, Name = "A Quiet Place", Rating = "7", ReleaseYear = 2018, Genre = "Horror" },
+            new MovieViewModel { Id = 7, Name = "John Wick", Rating = "8", ReleaseYear = 2023, Genre = "Action" },
+            new MovieViewModel { Id = 8, Name = "Kill Boksoon", Rating = "5", ReleaseYear = 2023, Genre = "Action" },
+            new MovieViewModel { Id = 9, Name = "Polite Society", Rating = "7", ReleaseYear = 2023, Genre = "Action" },
+            new MovieViewModel { Id = 10, Name = "The Mother", Rating = "8", ReleaseYear = 2023, Genre = "Action" },
+            new MovieViewModel { Id = 11, Name = "Barcelona", Rating = "5", ReleaseYear = 2016, Genre = "Romance" },
+            new MovieViewModel { Id = 12, Name = "Haunted Mansion", Rating = "7", ReleaseYear = 2023, Genre = "Fantasy" },
+            new MovieViewModel { Id = 13, Name = "Spy", Rating = "8", ReleaseYear = 2023, Genre = "Fantasy" },
+            new MovieViewModel { Id = 14, Name = "Barbie", Rating = "5", ReleaseYear = 2023, Genre = "Comedy" },
+            new MovieViewModel { Id = 15, Name = "Dream", Rating = "7", ReleaseYear = 2023, Genre = "Comedy" },
             };
         }
         public ActionResult Random()
